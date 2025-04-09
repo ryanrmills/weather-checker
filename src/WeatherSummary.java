55d903c437e3feba3ff99aff1c6334371e5a5f66
Auberon López <auberonedu@gmail.com>
2025-04-09 09:22:35 -0700

    Weather summary skeleton

---
+++ src/WeatherSummary.java
@@ -0,0 +1,22 @@
+public class WeatherSummary {
+    /**
+     * Reads newline-delimted temperatures from System.in and prints summary
+     * statistics to System.out.
+     * 
+     * Example input:
+     * 66.4
+     * 77.1
+     * 72.6
+     * 
+     * Example output:
+     * Max: 66.4
+     * Min: 77.1
+     * Average: 72.03333333333333
+     * 
+     * @param args command line arguments (ignored)
+     */
+    public static void main(String[] args) {
+        // Implement this method!
+        // Hint: use Scanner. nextDouble() and hasNextDouble() will be helpful here!
+    }
+}
