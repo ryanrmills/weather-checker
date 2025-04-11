4a8468ef2758b8fbc860057a73bc3c9a6ce3339d
ryanrmills <ryanroymills@gmail.com>
2025-04-11 15:35:59 -0700

    making sure WeatherSummary is working

--- src/WeatherSummary.java
+++ src/WeatherSummary.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class WeatherSummary {
     /**
      * Reads newline-delimted temperatures from System.in and prints summary
@@ -18,5 +20,16 @@ public class WeatherSummary {
     public static void main(String[] args) {
         // Implement this method!
         // Hint: use Scanner. nextDouble() and hasNextDouble() will be helpful here!
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("This part of the code is working.");
+
+        while (scan.hasNextDouble()){
+            double currTemp = scan.nextDouble();
+            System.out.println("Temperature: " + currTemp);
+        }
+
+        scan.close();
     }
 }
